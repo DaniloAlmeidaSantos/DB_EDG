360b9e4313ae75d103d9050aa6b07006349bcb94
Danilo Almeida <48030619+DaniloAlmeidaSantos@users.noreply.github.com>
2022-05-22 23:19:19 -0300

    Create insert_user&Prods.sql

---
+++ Utils/Scripts/insert_user&Prods.sql
@@ -0,0 +1,5 @@
+insert into clientes (NOME, CPF, TELEFONE, SEXO, RG, EMAIL, DATA_NASC, CEP, CIDADE, ESTADO, LOGRADOURO, BAIRRO, NUMERO, COMPLEMENTO) 
+values ("Danilo Almeida", "42409149812", "963273155", "MA", "52653268", "danilo8almeida@hotmail.com", 2000-12-21, "04865-070", "São Paulo", "SP", "Rua Forte do castelo", "Jardim Iporã", "222", "Casa");
+
+
+INSERT INTO produtos (NOME_PRODUTO, VALOR, KG, QUANTIDADE) VALUES ("Arroz", 50, 0, 50);
