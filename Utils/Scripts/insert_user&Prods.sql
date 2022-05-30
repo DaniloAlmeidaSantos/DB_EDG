0f8945347acb762136be1e30c1668c78d7cbdb33
Danilo Almeida <48030619+DaniloAlmeidaSantos@users.noreply.github.com>
2022-05-30 15:34:34 -0300

    Update insert_user&Prods.sql

--- Utils/Scripts/insert_user&Prods.sql
+++ Utils/Scripts/insert_user&Prods.sql
@@ -4,3 +4,5 @@ values ("Danilo Almeida", "42409149812", "963273155", "MA", "52653268", "danilo8
 
 INSERT INTO produtos (NOME_PRODUTO, VALOR, KG, QUANTIDADE, VALIDADE) VALUES ("Arroz", 50, 0, 50, '2023-12-21');
 
+INSERT INTO funcionarios(`ID_FUNCIONARIO`,`CPF`,`TELEFONE`,`SEXO`,`RG`,`EMAIL`,`DATA_NASC`,`CEP`,`CIDADE`,`ESTADO`,`LOGRADOURO`,`BAIRRO`,`NUMERO`,`CARGO`,`SENHA`) 
+VALUES(1,'55555555555','11555555555','MA','52','funcionario@edg.com','2000-12-21',04865070,'São Paulo','SP','Rua forte do castelo','Jardim Alusão','55','VENDEDOR','123456');
